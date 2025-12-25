c00f89a33a13015ed82a3089733d9b8491bcb8ee
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2025-12-26 04:01:18 +0600

    feat: Add HeartDiseaseDb database definition
    Introduces the `HeartDiseaseDb` class, a Floor database for local data persistence.
    
    This database is defined with version 1 and includes two entities:
    - `HealthAssessmentEntity`
    - `FactorContribution`
    
    It also provides Data Access Objects (DAOs) for these entities: `healthAssessmentDao` and `factorContributionDao`.

---
+++ lib/datasource/db/heart_disease_db.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:floor/floor.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+
+
+import '../../models/health_assessment.dart';
+import 'dao/factor_contribution_dao.dart';
+import 'dao/health_assesment_dao.dart';
+part 'heart_disease_db.g.dart';
+
+@Database(version: 1, entities: [HealthAssessmentEntity, FactorContribution])
+abstract class HeartDiseaseDb extends FloorDatabase {
+  HealthAssessmentDao get healthAssessmentDao;
+  FactorContributionDao get factorContributionDao;
+}
