4d0f2c7b8e8fa8c0f429b1e482ddc9591e245e2f
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2025-12-28 01:22:59 +0600

    feat: Define AuthenticationRepository interface
    Introduces the `AuthenticationRepository` abstract class to define the contract for authentication-related operations.
    
    This interface outlines three core methods:
    - `signInWithGoogle`: To handle user sign-in via Google.
    - `signOut`: To manage user sign-out.
    - `getUserProfile`: To fetch the profile of the authenticated user.

---
+++ lib/data/repository/authentication_repository.dart
@@ -0,0 +1,7 @@
+import 'package:heart_disease_prediction/models/user_profile.dart';
+
+abstract class AuthenticationRepository {
+  Future<void> signInWithGoogle();
+  Future<void> signOut();
+  Future<UserProfile> getUserProfile();
+}
