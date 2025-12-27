abde00214c38e1f6d52151a9f5ab287384d15fd9
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2025-12-28 01:22:53 +0600

    feat: Implement Google Sign-In and Sign-Out
    Introduces `AuthenticationRepositoryIml`, a new implementation of the `AuthenticationRepository` that handles user authentication via Google.
    
    This change adds the following functionality:
    - **`signInWithGoogle()`**: Implements the complete Google Sign-In flow. It authenticates the user with Google, obtains credentials, and then uses them to sign in to Firebase. Upon successful sign-in, it creates a `UserModel` and saves or merges the user's profile (UID, name, email) into the 'users' collection in Firestore.
    - **`signOut()`**: Implements sign-out logic by disconnecting from Google Sign-In and signing out from Firebase Auth.

---
+++ lib/data/repository_iml/authentication_repository_iml.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:heart_disease_prediction/models/user_profile.dart';
+
+import '../repository/authentication_repository.dart';
+
+class AuthenticationRepositoryIml extends AuthenticationRepository {
+  final FirebaseAuth _firebaseAuth;
+  final FirebaseFirestore _firestore;
+
+  AuthenticationRepositoryIml(this._firebaseAuth, this._firestore);
+  final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
+
+  @override
+  Future<UserProfile> getUserProfile() {
+    // TODO: implement getUserProfile
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signInWithGoogle() async {
+    try {
+      await _googleSignIn.initialize();
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await _googleSignIn
+          .authenticate();
+
+      if (googleUser == null) {
+        // User cancelled the login
+        throw Exception("Sign-in cancelled.");
+      }
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleAuth = googleUser.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken,
+      );
+
+      // Sign in to Firebase with the Google [UserCredential]
+      final UserCredential userCredential = await _firebaseAuth
+          .signInWithCredential(credential);
+      final User? user = userCredential.user;
+
+      if (user != null) {
+        final uid = user.uid;
+        final email = user.email;
+        final name = user.displayName;
+
+        final userModel = UserModel(uid: uid, name: name!, email: email!);
+        await _firestore
+            .collection('users')
+            .doc(uid)
+            .set(userModel.toMap(), SetOptions(merge: true));
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  Future<void> signOut() async {
+    try {
+      // if (await _googleSignIn.) {
+      // Google Sign-Out & Disconnect
+      await _googleSignIn.disconnect();
+      await _googleSignIn.signOut();
+      // 🔒 Optional: Revoke access
+      // }
+
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      throw Exception("Google Sign-Out Error: $e");
+    }
+  }
+}
