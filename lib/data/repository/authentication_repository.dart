b687988ae6b49e61b781f9a0ac4327c6e4d2ae5c
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2025-12-28 19:11:16 +0600

    refactor: Update getUserProfile return type to UserModel
    Changes the return type of the `getUserProfile` method in the `AuthenticationRepository` abstract class from `UserProfile` to `UserModel`.

--- lib/data/repository/authentication_repository.dart
+++ lib/data/repository/authentication_repository.dart
@@ -3,5 +3,5 @@ import 'package:heart_disease_prediction/models/user_profile.dart';
 abstract class AuthenticationRepository {
   Future<void> signInWithGoogle();
   Future<void> signOut();
-  Future<UserProfile> getUserProfile();
+  Future<UserModel> getUserProfile();
 }
