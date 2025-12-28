2549249ed2d408a01af8f3f36ba6d006ba891379
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2025-12-28 19:10:38 +0600

    refactor: Organize imports in database file
    Sorts and organizes the import statements in `lib/datasource/db/heart_disease_db.dart` for better readability and consistency. This change involves reordering existing imports and removing an unnecessary blank line.

--- lib/datasource/db/heart_disease_db.dart
+++ lib/datasource/db/heart_disease_db.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
+
 import 'package:floor/floor.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
-
 
 import '../../models/health_assessment.dart';
 import 'dao/factor_contribution_dao.dart';
 import 'dao/health_assesment_dao.dart';
+
 part 'heart_disease_db.g.dart';
 
 @Database(version: 1, entities: [HealthAssessmentEntity, FactorContribution])
