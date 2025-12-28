d00cdb78c859ebc703ac91b79e8b95e9c16bc23a
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2025-12-28 19:10:33 +0600

    refactor: Simplify HTML rendering import
    Removes the unused `flutter_html` package import from `heart_health_tips_screen.dart`.
    
    This change cleans up the file by keeping only the necessary `flutter_widget_from_html` import, which is actively used for rendering HTML content.

--- lib/screens/heart_health_tips_screen.dart
+++ lib/screens/heart_health_tips_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+
 import '../config/app_colors.dart';
 
 class HeartHealthTipsScreen extends StatelessWidget {
